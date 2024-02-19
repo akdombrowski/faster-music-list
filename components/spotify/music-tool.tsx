d29fa304137a8cc8d7d20e9d6020c53c86e37e27
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-02-18 18:39:50 -0600

    music tool layout

--- components/spotify/music-tool.tsx
+++ components/spotify/music-tool.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 export default function MusicTool() {
   return (
-    <h1>
-      music
-    </h1>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+
+        <h1>music</h1>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
